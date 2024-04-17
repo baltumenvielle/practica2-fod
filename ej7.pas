4e0e25bb632f5d3cce3527eb8fea18b715af0e2f
baltazarmenvielle <baltazarmenvielle@gmail.com>
2024-04-17 11:11:19 -0300

    Se agrego el ejercicio 7 funcionalidad completa

--- ej7.pas
+++ ej7.pas
@@ -29,17 +29,22 @@ begin
   if (not eof(detalle)) then
     read(detalle, m)
   else
-    m.codigo := valorAlto;
+    m.codigo_localidad := valorAlto;
 end;
 
 procedure minimo(var v: vectorRegistros; var min: municipio; var d: vectorDetalles);
 var
-  i, pos, minimo: integer;
+  i, pos, min1, min2: integer;
 begin
-  minimo := 9999;
+  min1 := 9999;
+  min2 := 9999;
   for i:=1 to 10 do begin
-    if (v[i].codigo < minimo) then begin
-      minimo := v[i].codigo;
+    if (v[pos].codigo_localidad <= min1) then begin
+      min2 := 9999;
+      min1 := v[i].codigo_localidad;
+    end;
+    if (v[i].codigo_localidad < min2) then begin
+      min2 := v[i].codigo_cepa;
       pos := i;
     end;
   end;
@@ -49,9 +54,35 @@ end;
 
 procedure actualizarMaestro(var maestro: master; var d: vectorDetalles: v: vectorRegistros);
 var
+  m: ministerio;
   min: municipio;
+  actual_cod, actual_cepa: integer;
 begin
   minimo(v, min, d);
+  while (min.codigo <> valorAlto) do begin
+    read(maestro, m);
+    while (m.codigo_localidad <> min.codigo_localidad) and (m.codigo_cepa <> min.codigo_cepa) do
+      read(maestro, m);
+    while (m.codigo_localidad = min.codigo_localidad) and (m.codigo_cepa = min.codigo_cepa) do begin
+      m.fallecidos := m.fallecidos + min.fallecidos;
+      m.recuperados := m.recuperados + min.recuperados;
+      m.casos_activos := min.casos_activos;
+      m.casos_nuevos := m.casos_nuevos;
+
+      minimo(v, min, d);
+    end;
+  end;
+end;
+
+procedure informarCasosActivos(var maestro: master);
+var
+  m: municipio;
+begin
+  while (not eof(maestro)) do begin
+    read(maestro, m);
+    if (m.casos_activos > 50) then
+      writeln('La localidad ', m.localidad, ' tiene mas de 50 casos activos');
+  end;
 end;
 
 var
